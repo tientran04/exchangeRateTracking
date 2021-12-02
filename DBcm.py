576dfc65a65c8b0ecda5e18a694e4f3a1de23560
tientran04 <66058866+tientran04@users.noreply.github.com>
2021-12-02 22:10:31 +1300

    Update DBcm.py

--- DBcm.py
+++ DBcm.py
@@ -7,12 +7,20 @@ class UseDatabase:
         self.configuration = config
 
     def __enter__(self):
-        try:
-            self.conn = psycopg2.connect(**self.configuration)
-            self.cursor = self.conn.cursor()
-            return self.cursor
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
+        if type(self.configuration) is str:
+            try:
+                self.conn = psycopg2.connect(self.configuration)
+                self.cursor = self.conn.cursor()
+                return self.cursor
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+        else:
+            try:
+                self.conn = psycopg2.connect(**self.configuration)
+                self.cursor = self.conn.cursor()
+                return self.cursor
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
 
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
         self.conn.commit()
